707c28931a88d9612d250e4c33eca5abfe07b634
Conversion Rate Pros <146614093+conversionratepros@users.noreply.github.com>
2025-11-10 20:49:56 +0200

    AddOn Pages added

--- OnePlanPetJourney/obj/Debug/net9.0/OnePlanPetJourney.AssemblyInfo.cs
+++ OnePlanPetJourney/obj/Debug/net9.0/OnePlanPetJourney.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnePlanPetJourney")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81a273c0b13491c4a9e14b89c8d3d2671ba05f09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+445d32d218f7c343d4b50393ab5492a6c0754d5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnePlanPetJourney")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnePlanPetJourney")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
